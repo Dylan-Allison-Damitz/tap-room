c00e1c5b84c934bc2d64a6566b6f9fff8b318799
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2022-01-15 14:34:07 -0800

    add initial beer detail functionality

---
+++ src/components/BeerDetail.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+function BeerDetail(props) {
+  const { beer, onClickingDelete, onClickingBuy, onClickingRestock } = props;
+  return (
+    <React.Fragment>
+      <h1>Beer Detail</h1>
+      <h3>
+        {beer.description} - {beer.name}
+      </h3>
+      <p>
+        <em>{beer.quantity}</em>
+      </p>
+      <button onClick={props.onClickingEdit}>UpdateBeer</button>
+      <button onClick={() => onClickingDelete(beer.id)}>Remove Beer</button>
+      <button onClick={() => onClickingBuy(beer.id)}>Buy</button>
+      <button onClick={() => onClickingRestock(beer.id)}>Restock</button>
+      <hr />
+    </React.Fragment>
+  );
+}
